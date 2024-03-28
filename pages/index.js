110a782668dae69c0bf809b863f19a7696635e29
Robert Leifke <r.leifke@gmail.com>
2024-03-28 19:59:56 -0400

    fix: bio

--- pages/index.js
+++ pages/index.js
@@ -10,10 +10,9 @@ export default function Home() {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Heyo! My name is Robert Leifke. I am building {' '}
-        <a href="https://powermaker.org/">PowerMaker</a>, an automated market maker for <a href="https://www.paradigm.xyz/2021/08/power-perpetuals">power perpetuals</a> on Ethereum.</p>
+        <a href="https://numo.trade/">Numo</a>, is a replicating market maker for <a href="https://www.paradigm.xyz/2021/08/power-perpetuals">power perpetuals</a> on Ethereum.</p>
         <p>
-          My ultimate goal is to create a unified marketplace for convexity on the blockchain. My hope is PowerMaker can be the protocol that enables this.
-          I think it would do our society a lot of <a href="https://youtu.be/FGkP63hJivQ?si=VuAaAHECTZj2jik4">good</a>.</p>
+          My ultimate goal with Numo is to create a unified marketplace for convexity on the blockchain. A world where financial system lives onchain, would do our society a lot of <a href="https://youtu.be/FGkP63hJivQ?si=VuAaAHECTZj2jik4">good</a>.</p>
         <p>
           For now, I'm on a leave of absence from the University of Michigan where I'm majoring in Mathematics. My 
           research interests are in <a href="https://arxiv.org/pdf/2308.08066.pdf">Constant Function Market Makers</a>, 
