a8c8d88805adb729eae91320f74c08134b108956
Robert Leifke <44106773+robertleifke@users.noreply.github.com>
2025-02-06 12:37:50 -0500

    chore: update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,11 +10,11 @@ export default function Home() {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Heyo! My name is Robert Leifke. I am working on {' '}
-        <a href="https://x.com/numocash/">Numo</a>, an automated market maker (AMM) for hedging foreign currrencies on blockchains.</p>
+        <a href="https://x.com/numocash/">Numo</a>, an automated market maker (AMM) for efficient exchange and hedging of foreign currrencies on blockchains.</p>
         <p>It's implementation is inspired by the paper, 
-        <a href="https://arxiv.org/abs/2103.14769"><em> replicating market makers</em></a> which first introduced the idea of AMMs for hedging and not just spot trading.</p>
+        <a href="https://arxiv.org/abs/2103.14769"><em> replicating market makers</em></a> which first introduced the idea of AMMs for hedging.</p>
         <p>
-          With Numo I hope to create the most liquid marketplace for foreign currencies in the world.</p>
+          With Numo, I aim to create a seamless, global payments network. One that instantly connects all currencies and removes exchange rate risk.</p>
         <p>
           I'm finishing my degree in Mathematics from the University of Michigan. My 
           research interests are in <a href="https://arxiv.org/pdf/2308.08066.pdf">automated market makers</a> and 
